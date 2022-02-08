097284fb96149f12c1f66dd70d1409b9b0cfee68
kaporter <kailiporter@hotmail.com>
2022-02-08 16:24:32 -0600

    added basic classes

---
+++ main.py
@@ -0,0 +1,6 @@
+# imports
+import random
+
+import map
+
+areaMap = map.Map(200, .2, 1000)
---
+++ vehicle.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    '''
+        Variables: array of customers(capacity=5), current location, next destination (path?)
+    '''
+    def __init__(self):
+
+class Customer:
+    '''
+        Variables: pick-up point, drop-off location,
+    '''
+    def __init__(self):
