8fafaedef9f17186099137508bebcddbf861e038
kaporter <kailiporter@hotmail.com>
2022-02-11 17:03:30 -0600

    added some details to basic classes

--- main.py
+++
@@ -1,6 +0,0 @@
-# imports
-import random
-
-import map
-
-areaMap = map.Map(200, .2, 1000)
--- map.py
+++ map.py
@@ -8,13 +8,12 @@ class Map:
     def __init__(self, nodes, connectivity, seed):
         self.seed = seed
         self.graph = nx.gnp_random_graph(nodes, connectivity, seed=seed)
-        self.directedGraph = nx.DiGraph(self.graph)
         self.color = 'lightblue'
 
     def change_color(self, color):
         self.color = color
 
-    def display_graph(self):
+    def display_graph(self): # needs work - does not show correctly
         pos = nx.spring_layout(self.graph, seed=self.seed, k=1.0)
         nx.draw_spring(self.graph, node_size=500, node_color=self.color, linewidths=0.25,
         font_size=10, font_weight='bold', with_labels=True)
